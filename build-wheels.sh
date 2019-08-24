b203ad1dfb4cbc4f24f2459bfb3f68ea2ed46366
Alex Forrence <aforren1@jhu.edu>
2019-08-24 13:32:17 -0400

    initial

---
+++ build-wheels.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e -x
+
+# Install a system package required by our library
+#yum install -y atlas-devel
+git --git-dir=/ptb/.git submodule update --init --recursive
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    #"${PYBIN}/pip" install -r /ptb/dev-requirements.txt
+    "${PYBIN}/pip" wheel /ptb/psychtoolbox-3/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /ptb/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install psychtoolbox --no-index -f /ptb/wheelhouse
+    #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+done
