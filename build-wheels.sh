9f69c9b7721c199fe05b7b55c14112509ac2526f
Alex Forrence <aforren1@jhu.edu>
2019-08-26 10:27:02 -0400

    hmm

--- build-wheels.sh
+++ build-wheels.sh
@@ -5,7 +5,7 @@ set -e -x
 yum install -y libusb1-devel portaudio-devel libXi-devel alsa-lib-devel
 git --git-dir=/ptb/.git submodule update --init --recursive
 
-pyvs=(cp37-cp37m)
+pyvs=(cp36-cp36m)
 # Compile wheels
 for PYBIN in "${pyvs[@]}"; do
     "/opt/python/${PYBIN}/bin/pip" install numpy
@@ -20,7 +20,8 @@ done
 # Install packages and test
 for PYBIN in "${pyvs[@]}"; do
     "/opt/python/${PYBIN}/bin/pip" install psychtoolbox --no-index -f /ptb/wheelhouse
-    ldd "/opt/python/cp37-cp37m/lib/python3.7/site-packages/psychtoolbox/PsychPortAudio.cpython-37m-x86_64-linux-gnu.so"
+    ls "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/"
+    ldd "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/PsychPortAudio.cpython-36m-x86_64-linux-gnu.so"
     "/opt/python/${PYBIN}/bin/python" -c "import psychtoolbox as ptb; print(ptb.GetSecs())"
     #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
