77b1aa80cd1ba482d8436efedc98e5d42981a029
Alex Forrence <aforren1@jhu.edu>
2019-08-26 11:38:17 -0400

    try manual portaudio

--- build-wheels.sh
+++ build-wheels.sh
@@ -2,7 +2,16 @@
 set -e -x
 
 # Install a system package required by our library
-yum install -y libusb1-devel portaudio-devel libXi-devel alsa-lib-devel
+yum install -y libusb1-devel libXi-devel alsa-lib-devel
+
+# build portaudio manually
+git clone https://git.assembla.com/portaudio.git
+cd portaudio
+git checkout pa_stable_v190600_20161030
+./configure && make
+sudo make install
+cd ..
+
 git --git-dir=/ptb/.git submodule update --init --recursive
 
 pyvs=(cp36-cp36m)
@@ -22,7 +31,7 @@ for PYBIN in "${pyvs[@]}"; do
     "/opt/python/${PYBIN}/bin/pip" install psychtoolbox --no-index -f /ptb/wheelhouse --upgrade
     ls "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/"
     ldd "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/PsychPortAudio.cpython-36m-x86_64-linux-gnu.so"
-    readelf -Ws "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/.libs/libportaudio-126f40f2.so.2.0.0"
+    #readelf -Ws "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/.libs/libportaudio-126f40f2.so.2.0.0" | grep PaAlsa
     readelf -Ws "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/PsychPortAudio.cpython-36m-x86_64-linux-gnu.so" | grep PaAlsa
     "/opt/python/${PYBIN}/bin/python" -c "import psychtoolbox as ptb; print(ptb.GetSecs())"
     #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
