8908bbd260bc318f0a2af1c7ec819d53af489d88
Alex Forrence <aforren1@jhu.edu>
2019-08-26 11:57:44 -0400

    Wrong remote (I think), add 3.7 back to builds

--- build-wheels.sh
+++ build-wheels.sh
@@ -14,11 +14,10 @@ cd ..
 
 git --git-dir=/ptb/.git submodule update --init --recursive
 
-pyvs=(cp36-cp36m)
+pyvs=(cp36-cp36m cp37-cp37m)
 # Compile wheels
 for PYBIN in "${pyvs[@]}"; do
     "/opt/python/${PYBIN}/bin/pip" install numpy
-    "/opt/python/${PYBIN}/bin/pip" wheel /ptb/psychtoolbox-3/ -w wheelhouse/ -v
 done
 
 # Bundle external shared libraries into the wheels
@@ -28,11 +27,7 @@ done
 
 # Install packages and test
 for PYBIN in "${pyvs[@]}"; do
-    "/opt/python/${PYBIN}/bin/pip" install psychtoolbox --no-index -f /ptb/wheelhouse --upgrade
-    ls "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/"
-    ldd "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/PsychPortAudio.cpython-36m-x86_64-linux-gnu.so"
-    #readelf -Ws "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/.libs/libportaudio-126f40f2.so.2.0.0" | grep PaAlsa
-    readelf -Ws "/opt/python/cp36-cp36m/lib/python3.6/site-packages/psychtoolbox/PsychPortAudio.cpython-36m-x86_64-linux-gnu.so" | grep PaAlsa
+    "/opt/python/${PYBIN}/bin/pip" install psychtoolbox --no-index -f /ptb/wheelhouse
     "/opt/python/${PYBIN}/bin/python" -c "import psychtoolbox as ptb; print(ptb.GetSecs())"
     #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
