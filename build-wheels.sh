133bd1698d248d1eb5c1dfed52ca0c53dffc175b
Alex Forrence <aforren1@jhu.edu>
2019-08-26 12:14:52 -0400

    better notes, switch back to fork for now

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e -x
 
-# Install a system package required by our library
+# Install system packages required by our library
 yum install -y libusb1-devel libXi-devel alsa-lib-devel
 
-# build portaudio manually
+# build portaudio manually (couldn't find PaAlsa_SetNumPeriods symbol in the system package)
 git clone https://git.assembla.com/portaudio.git
 cd portaudio
 git checkout pa_stable_v190600_20161030
@@ -17,7 +17,6 @@ git --git-dir=/ptb/.git submodule update --init --recursive
 pyvs=(cp36-cp36m cp37-cp37m)
 # Compile wheels
 for PYBIN in "${pyvs[@]}"; do
-    "/opt/python/${PYBIN}/bin/pip" install numpy
     "/opt/python/${PYBIN}/bin/pip" wheel /ptb/psychtoolbox-3/ -w wheelhouse/ -v
 done
 
@@ -30,5 +29,4 @@ done
 for PYBIN in "${pyvs[@]}"; do
     "/opt/python/${PYBIN}/bin/pip" install psychtoolbox --no-index -f /ptb/wheelhouse
     "/opt/python/${PYBIN}/bin/python" -c "import psychtoolbox as ptb; print(ptb.GetSecs())"
-    #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
