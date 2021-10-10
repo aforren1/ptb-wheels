5aa3460b9198e9ce278e321081234685bed85858
Alex Forrence <alex.forrence@gmail.com>
2021-10-10 14:30:00 -0400

    Prepare for new PTB release.
    Switch back to submodule (we can just push the right _version.py upstream),
    switch to bundling portaudio v19.7.0, and try to build universal wheels for Mac M1.

--- prebuild_linux.sh
+++ prebuild_linux.sh
@@ -9,7 +9,7 @@ then
     # build portaudio manually (couldn't find PaAlsa_SetNumPeriods symbol in the system package)
     git clone https://github.com/portaudio/portaudio.git
     cd portaudio
-    git checkout pa_stable_v190600_20161030
+    git checkout v19.7.0
     ./configure && make
     make install
     cd ..
