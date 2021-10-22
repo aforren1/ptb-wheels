bdcc4f41e181a10a49a1205a9efe3d1fb538afbe
Alex Forrence <alex.forrence@gmail.com>
2021-10-22 18:01:03 -0400

    for the walk

--- prebuild_linux.sh
+++ prebuild_linux.sh
@@ -5,9 +5,9 @@ if [ ! -d "portaudio/" ]; # check if we've run this already
 then
     # Install system packages required by psychtoolbox
     yum install -y libusb1-devel libXi-devel alsa-lib-devel \
-      jack-audio-connection-kit-devel gstreamer1-devel \
+      jack-audio-connection-kit-devel \
       mesa-libGL-devel mesa-libGLU-devel freeglut freeglut-devel xorg-x11-server-devel \
-      libXrandr-devel libXcursor-devel gstreamer-plugins-base-devel
+      libXrandr-devel libXcursor-devel gstreamer1-plugins-base-devel
 
     # build portaudio manually (couldn't find PaAlsa_SetNumPeriods symbol in the system package)
     git clone https://github.com/portaudio/portaudio.git
