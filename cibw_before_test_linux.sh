f30ed6b9bf65508527ef4a9c6a4eb5639350e55a
Eric Larson <larson.eric.d@gmail.com>
2024-07-29 14:03:29 -0400

    FIX: FINE

--- cibw_before_test_linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -exo pipefail
-
-yum install -y pulseaudio pulseaudio-utils alsa-utils
-sudo modprobe snd-dummy || true
-pulseaudio --start
-pactl list
-aplay -L
--- cibw_test_command.py
+++ cibw_test_command.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
-import psychtoolbox as ptb
+import os
 import pprint
+
+import psychtoolbox as ptb
 
 t = ptb.GetSecs()
 assert t > 0, t
 
 from psychtoolbox import audio  # noqa: F401
 
-pprint.pprint(audio.get_devices())
-assert audio.get_devices()
+# Despite putting this in cibw_before_test_linux.sh:
+#
+# yum install -y pulseaudio pulseaudio-utils alsa-utils
+# sudo modprobe snd-dummy || true
+# pulseaudio --start
+# pactl list
+# aplay -L
+#
+# The CIBW Linux docker instance audio device is not avaialble.
+
+if os.getenv("PTB_SOUND_TEST").lower() not in ("false", "0"):
+    pprint.pprint(audio.get_devices())
+    assert audio.get_devices()
