d737b02003c4cd0ed5bf95745c06b667b0a527eb
Eric Larson <larson.eric.d@gmail.com>
2024-07-29 11:43:30 -0400

    FIX: Tests

---
+++ cibw_test_command.py
@@ -0,0 +1,10 @@
+import psychtoolbox as ptb
+import pprint
+
+t = ptb.GetSecs()
+assert t > 0, t
+
+from psychtoolbox import audio  # noqa: F401
+
+pprint.pprint(audio.get_devices())
+assert audio.get_devices()
--- cibw_test_command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -exo pipefail
-
-python -c "import psychtoolbox as ptb; print(ptb.GetSecs())"
