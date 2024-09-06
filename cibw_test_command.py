0c7dbec51321b3e57cb850c889269d57c448cef3
Alex Forrence <alexander.forrence@yale.edu>
2024-09-06 09:17:18 -0400

    test wheels on macos-13 in addition to 12, and test libusb functionality

--- cibw_test_command.py
+++ cibw_test_command.py
@@ -23,3 +23,6 @@ from psychtoolbox import audio  # noqa: F401
 if os.getenv("PTB_SOUND_TEST", "").lower() not in ("false", "0"):
     pprint.pprint(audio.get_devices())
     assert audio.get_devices()
+
+# test that libusb is found
+assert(ptb.PsychHID('OpenUSBDevice', -1, -1) == 1)
